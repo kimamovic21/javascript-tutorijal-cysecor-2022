8742d5010909c27f559f4734707dbd9f69971e68
kimamovic21 <imamovic.kerim@gmail.com>
2022-08-16 15:26:34 +0200

    izmjene
    index.html fajl
    script.js fajl

---
+++ script.js
@@ -0,0 +1,89 @@
+// alert('Hello world!');
+// console.log('Hello world!');
+// document.write('Hello world!');
+// window.alert('Hello world!');
+// console.error('Hello world!')
+// console.warn('Hello world');
+// console.log(1+1);
+// console.log(1===1);
+// console.log(1===2);
+//window.open();
+
+// Ovo je komentar u jednoj liniji
+/*Ovo je 
+komentar
+u vise linija
+*/
+
+// document.getElementById('poruka').innerText = 'Poruka iz JS fajla';
+
+// Ovo je kod koji ce se ispisati u konzoli
+// console.log('test');
+
+/*
+Ovo je kod koji ce da uzme div element preko njegovog id-a i onda ce
+da mu upise ovaj tekst.
+*/
+// document.getElementById('poruka').innerText = 'Poruka iz JS fajla';
+
+
+
+// Deklarisanje varijable
+// Deklarizacija i inicijalizacija
+
+// let kerim;
+// kerim = 'Ja se zovem Kerim Imamovic';
+// console.log(kerim);
+
+// let trenutnaGodina = 2022;
+// console.log(trenutnaGodina);
+
+// let godinaRodenja = 1996;
+// console.log(godinaRodenja);
+// let ukupnoGodina = trenutnaGodina - godinaRodenja;
+// console.log(ukupnoGodina);
+
+// console.log(typeof(ukupnoGodina));
+
+
+// let prvi_broj = 1;
+// let drugi_broj = '2';
+// drugi_broj = Number(drugi_broj);
+// let rezultat = prvi_broj + drugi_broj;
+// console.log(rezultat);
+
+
+// let prvi_broj = 5;
+// let drugi_broj = 10;
+
+// console.log(prvi_broj);
+// console.log(drugi_broj);
+
+// prvi_broj = drugi_broj;
+// drugi_broj = prvi_broj;
+
+// console.log(prvi_broj);
+// console.log(drugi_broj);
+
+
+// Zamjena brojeva
+
+// let prvi_broj = 5;
+// let drugi_broj = 10;
+// let treci_broj;
+
+// treci_broj = prvi_broj; //5
+// prvi_broj = drugi_broj; //10
+// drugi_broj = treci_broj; //5
+
+// console.log(prvi_broj);
+// console.log(drugi_broj);
+
+// let prvi_broj = 5;
+// const drugi_broj = 10;
+
+// drugi_broj = prvi_broj;
+// console.log(drugi_broj);
+
+// video 55:20
+
