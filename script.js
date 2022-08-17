9d2f61259d10c0e4b71fb85b625f0e8b49b1acca
kimamovic21 <imamovic.kerim@gmail.com>
2022-08-17 16:10:21 +0200

    izmjene
    script.js fajl

--- script.js
+++ script.js
@@ -85,5 +85,196 @@ da mu upise ovaj tekst.
 // drugi_broj = prvi_broj;
 // console.log(drugi_broj);
 
-// video 55:20
 
+
+// let ime = 'Kerim';
+// let prezime = 'Imamovic';
+// let godina = 26;
+
+// console.log(ime);
+// console.log(prezime);
+// console.log(godina);
+
+// let recenica = ('Moje ime je' + ' ' + ime + '.' + ' ' + 'Moje prezime je' + ' ' + prezime + '.' + ' ' + 'Imam' + ' ' + godina + ' ' + 'godina' + '.');
+// console.log(recenica);
+
+//console.log('Moje ime je' + ' ' + ime + '.' + ' ' + 'Moje prezime je' + ' ' + prezime + '.' + 'Imam' + ' ' + godina + ' ' + 'godina' + '.');
+
+// console.log('Ove godine imam ' + godina + ' godina.');
+
+//godina = godina - 1;
+// godina -= 1;
+// console.log('Prosle godine sam imao ' + godina + ' godina.');
+
+// godina = godina + 2;
+// godina += 2;
+// console.log('Sljedece godine cu imati ' + godina + ' godina.');
+
+
+
+// Backtick
+
+// let ime = 'Kerim';
+// let prezime = 'Imamovic';
+// let godina = 26;
+
+// let recenica = `Zovem se ${ime}. Prezivam se ${prezime}. Imam ${godina} godina.`;
+
+// console.log(recenica);
+
+
+
+// Logicki operatori
+
+// let prvi_broj = 10;
+// let drugi_broj = 5;
+// let treci_broj = 7;
+// let cetvrti_broj = 8;
+
+// console.log(prvi_broj > drugi_broj);
+// console.log(treci_broj > cetvrti_broj);
+
+// console.log((prvi_broj > drugi_broj) && (treci_broj > cetvrti_broj));
+// console.log((prvi_broj > drugi_broj) && (cetvrti_broj > treci_broj)); 
+
+// console.log((prvi_broj > drugi_broj) || (treci_broj > cetvrti_broj));
+// console.log((drugi_broj > prvi_broj) || (treci_broj > cetvrti_broj));
+
+
+
+
+// Uslovi (if / switch)
+
+// let prvi_broj = 5;
+// let drugi_broj = 10;
+
+// if(prvi_broj < drugi_broj) {
+//     console.log('Prvi broj je manji od drugog broja.');
+// }
+// else {
+//     console.log('Prvi broj nije manji od drugog broja.');
+// }
+
+
+// let vrijeme = 26;
+
+// if (vrijeme < 10) {
+//     console.log('Dobro jutro')
+// }
+// else if (vrijeme < 20) {
+//     console.log('Dobar dan');
+// }
+// else {
+//     console.log('Dobar vece')
+// }
+
+
+
+// let mjesec = 12;
+
+// if (mjesec === 1) {
+//     console.log('Januar');
+// }
+// else if (mjesec === 2) {
+//     console.log('Februar');
+// } 
+// else if (mjesec === 3) {
+//     console.log('Mart');
+// }
+// else if (mjesec === 4) {
+//     console.log('April');
+// }
+// else if (mjesec === 5) {
+//     console.log('Maj');
+// } 
+// else if (mjesec === 6) {
+//     console.log('Juni');
+// }
+// else if (mjesec === 7) {
+//     console.log('Juli');
+// }
+// else if (mjesec === 8) {
+//     console.log('August');
+// }
+// else if (mjesec === 9) {
+//     console.log('Septembar');
+// }
+// else if (mjesec === 10) {
+//     console.log('Oktobar');
+// } 
+// else if (mjesec === 11) {
+//     console.log('Novembar');
+// }
+// else if (mjesec === 12) {
+//     console.log('Decembar');
+// }
+// else {
+//     console.error ('Unesite broj izmedu 0 i 12');
+// }
+ 
+
+
+// let mjesec = 2;
+
+// switch(mjesec) {
+//     case 1:
+//         console.log('Januar');
+//         break;
+//     case 2:
+//         console.log('Feb');
+//     break;
+//     case 3:
+//         console.log('Mar');
+//     break;
+//     case 4:
+//         console.log('Apr');
+//     break;
+//     case 5:
+//         console.log('Maj');
+//     break;
+//     case 6:
+//         console.log('Jun');
+//     break;
+//     case 7:
+//         console.log('Jul');
+//     break;
+//     case 8:
+//         console.log('Aug');
+//     break;
+//     case 9:
+//         console.log('Okt');
+//     break;
+//     case 10:
+//         console.log('Sept');
+//     break;
+//     case 11:
+//         console.log('Nov');
+//     break;
+//     case 12:
+//         console.log('Dec');
+//     break;
+//     default: 
+//         console.error('Unijeti broj izmedu 0 i 12');
+// }
+
+
+// Petlje
+
+// 25 novcaniva
+let novcanice = 25; 
+// 10 Eura vrijednost jedne novcanice
+let vrijednostNovcanice = 10; 
+
+// izbrojane novcanice
+let izbrojaneNovcanice = 0;
+
+// broji novcanice sve dok je brojac manji od broja novcanica
+while (izbrojaneNovcanice < novcanice) {
+    izbrojaneNovcanice++;
+}
+console.log(izbrojaneNovcanice * vrijednostNovcanice);
+
+
+
+// Petlje i nizovi 
+// 01:27:10 video
